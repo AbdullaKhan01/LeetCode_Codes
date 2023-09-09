f7bd5ef1e26e290891cf218d422b2503adea17bd
Abdulla Khan <115298492+AbdullaKhan01@users.noreply.github.com>
2023-09-09 18:43:35 +0530

    9th August 2023

---
+++ 39. Combination Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void solve(vector<int>& candidates, int target,vector<vector<int>> &ans,vector<int> &temp,int index) {
+        if(target < 0){
+            return ;
+        }
+        if(target == 0){
+            ans.push_back(temp);
+        }
+        for(int i=index;i<candidates.size();i++){
+            temp.push_back(candidates[i]);
+            solve(candidates,target-candidates[i],ans,temp,i);
+            //backtraking;
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(candidates,target,ans,temp,0);
+        return ans;
+    }
+};
