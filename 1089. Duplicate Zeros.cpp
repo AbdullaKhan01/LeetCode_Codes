ceb1903b9ff8ab919261ea676660b156ac68732c
Abdulla Khan <115298492+AbdullaKhan01@users.noreply.github.com>
2023-09-20 19:29:13 +0530

    20th September 2023

---
+++ 1089. Duplicate Zeros.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int s = arr.size();
+        for(int i=0;i<s;i++){
+            if(arr[i] == 0){
+                arr.insert(arr.begin()+i,0);
+                i++;
+            }
+        }
+        while(arr.size()>s)
+            arr.pop_back();
+    }
+};
